8c9dd7c7b126ce821972ec28f3a5694483c7d0a7
Sebastian Webber <sebastian@swebber.me>
2018-01-15 09:25:21 -0200

    testing is valid

--- common_test.go
+++ common_test.go
@@ -36,3 +36,43 @@ func TestNew(t *testing.T) {
 		})
 	}
 }
+
+func TestCommand_IsValid(t *testing.T) {
+	type fields struct {
+		Command string
+		Args    []string
+		Options Options
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   bool
+	}{
+		{
+			name: "invalid",
+			fields: fields{
+				Command: "",
+			},
+			want: false,
+		},
+		{
+			name: "valid",
+			fields: fields{
+				Command: "ls",
+			},
+			want: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := &Command{
+				Command: tt.fields.Command,
+				Args:    tt.fields.Args,
+				Options: tt.fields.Options,
+			}
+			if got := c.IsValid(); got != tt.want {
+				t.Errorf("Command.IsValid() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
