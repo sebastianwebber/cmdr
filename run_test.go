b151e427e238a61b45471cfc0fc5e4e9a9598c95
Sebastian Webber <sebastian@swebber.me>
2018-01-15 09:38:27 -0200

    updating timeout

--- run_test.go
+++ run_test.go
@@ -21,7 +21,7 @@ func TestRunCmd(t *testing.T) {
 		{name: "run valid command with wrong args", cmd: "ls", args: []string{"-lh52313252362336", "2133266324"}, expectedError: "Error running a command"},
 		{name: "this should be ok", cmd: "ls", args: []string{"-lh"}},
 		{name: "this should be ok on bash too", cmd: "ls", args: []string{"-lh"}, useShell: true},
-		{name: "this should abort by timemout", cmd: "sleep", args: []string{"5"}, timeout: 2, expectedError: "Error running a command"},
+		{name: "this should abort by timemout", cmd: "sleep", args: []string{"2"}, timeout: 1, expectedError: "Error running a command"},
 	}
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
