1000fa3f9038dbbb605b5502fc78f19d5f2bf299
Sebastian Webber <sebastian@swebber.me>
2018-01-14 22:18:19 -0200

    add test for shell usage

--- run_test.go
+++ run_test.go
@@ -12,11 +12,13 @@ func TestRunCmd(t *testing.T) {
 		args          []string
 		expectedError string
 		timeout       int
+		useShell      bool
 	}{
 		{name: "run empty command", cmd: "", expectedError: "Missing command name"},
 		{name: "run fake command", cmd: "xl8t23hj6__68235896253gf3", expectedError: "Command not found in PATH"},
 		{name: "run valid command with wrong args", cmd: "ls", args: []string{"-lh52313252362336", "2133266324"}, expectedError: "Error running a command"},
 		{name: "this should be ok", cmd: "ls", args: []string{"-lh"}},
+		{name: "this should be ok on bash too", cmd: "ls", args: []string{"-lh"}, useShell: true},
 		{name: "this should abort by timemout", cmd: "sleep", args: []string{"5"}, timeout: 2, expectedError: "Error running a command"},
 	}
 	for _, test := range tests {
@@ -27,6 +29,11 @@ func TestRunCmd(t *testing.T) {
 			if test.timeout > 0 {
 				cmd.Options.Timeout = test.timeout
 			}
+
+			if test.useShell {
+				cmd.Options.UseShell = test.useShell
+			}
+
 			_, err := RunCmd(cmd)
 
 			if test.expectedError != "" {
