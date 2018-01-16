3bac9a7d1ccd1c9d33e34f9d52f765fa6c63ec3e
Eduardo Bohrer <nbluisrs@gmail.com>
2018-01-16 09:18:50 -0200

    Refactor Strategy to keep function inside him, so we dont need revalidate this type to execute.

--- group.go
+++ group.go
@@ -4,15 +4,29 @@ import (
 	"fmt"
 )
 
+func abortStrategy(cmdList []Command) (execCount int, err error) {
+	for _, cmd := range cmdList {
+		_, cmdErr := cmd.Run()
+
+		if cmdErr != nil {
+			err = fmt.Errorf("Error running a command: %v", cmdErr)
+			break
+		}
+
+		execCount++
+	}
+	return
+}
+
 // Strategy contains details about action to take when a
 // command fail inside a group execution
-type Strategy string
+type Strategy (func([]Command) (int, error))
 
-const (
+var (
 	// AbortOnError is a strategy to run grouped commands
 	// This stategy end the group execution resulting a error and output.
 	// It is the default option.
-	AbortOnError Strategy = "ABORT"
+	AbortOnError Strategy = abortStrategy
 
 	// // IgnoreOnError is a strategy to run grouped commands
 	// // This strategy ignores the problem
@@ -22,27 +36,5 @@ const (
 // Group allows to run lots of commands in sequence
 // The strategy defines the behavior when a error occours
 func Group(strategy Strategy, cmdList ...Command) (execCount int, err error) {
-
-	var rFunc (func([]Command) (int, error))
-
-	// if strategy == AbortOnError {
-	rFunc = abortGroup
-	// }
-
-	return rFunc(cmdList)
-}
-
-func abortGroup(cmdList []Command) (execCount int, err error) {
-	for _, cmd := range cmdList {
-		_, cmdErr := cmd.Run()
-
-		if cmdErr != nil {
-			err = fmt.Errorf("Error running a command: %v", cmdErr)
-
-			break
-		}
-
-		execCount++
-	}
-	return
+	return strategy(cmdList)
 }
