32a17d466b32de158088cf2b05ca666813305b90
Sebastian Webber <sebastian@swebber.me>
2018-04-12 12:01:15 -0300

    add a complex sed example

--- parse_test.go
+++ parse_test.go
@@ -16,6 +16,7 @@ func TestParse(t *testing.T) {
 		{name: "simple double quotes", cmd: `echo "hello world"`, wantOut: &Command{Command: "echo", Args: []string{"hello world"}}},
 		{name: "complex single quotes", cmd: `psql -U postgres -c 'select now() as "agora"'`, wantOut: &Command{Command: "psql", Args: []string{"-U", "postgres", "-c", `select now() as "agora"`}}},
 		{name: "complex double quotes", cmd: `psql -U postgres -c "select 'seba' as nome"`, wantOut: &Command{Command: "psql", Args: []string{"-U", "postgres", "-c", `select 'seba' as nome`}}},
+		{name: "sed complex", cmd: `sed -i 's|-Xmx.*|-Xmx3G|' teste.sh`, wantOut: &Command{Command: "sed", Args: []string{"-i", "s|-Xmx.*|-Xmx3G|", "teste.sh"}}},
 		{name: "exception double quotes", cmd: `/opt/wildfly-10.1.0.Final/bin/jboss-cli.sh -c --command="read-attribute server-state"`, wantOut: &Command{Command: "/opt/wildfly-10.1.0.Final/bin/jboss-cli.sh", Args: []string{`-c`, `--command="read-attribute server-state"`}}},
 	}
 	for _, tt := range tests {
