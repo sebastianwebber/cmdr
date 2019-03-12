15fad6e09b50df723f1b0ea649927d0fd7664765
Sebastian Webber <sebastian@swebber.me>
2019-03-12 15:26:04 -0300

    fix tests

--- run_test.go
+++ run_test.go
@@ -13,28 +13,21 @@ func TestRunCmd(t *testing.T) {
 		cmd           string
 		args          []string
 		expectedError string
-		timeout       int
-		useShell      bool
+		Options
 	}{
-		{name: "run empty command", cmd: "", expectedError: "Missing command name"},
-		{name: "run fake command", cmd: "xl8t23hj6__68235896253gf3", expectedError: "Command not found in PATH"},
+		{name: "run empty command", cmd: "", expectedError: "Missing command name", Options: Options{CheckPath: true}},
+		{name: "run fake command", cmd: "xl8t23hj6__68235896253gf3", expectedError: "Command not found in PATH", Options: Options{CheckPath: true}},
 		{name: "run valid command with wrong args", cmd: "ls", args: []string{"-lh52313252362336", "2133266324"}, expectedError: "Error running a command"},
 		{name: "this should be ok", cmd: "ls", args: []string{"-lh"}},
-		{name: "this should be ok on bash too", cmd: "ls", args: []string{"-lh"}, useShell: true},
-		{name: "this should abort by timemout", cmd: "sleep", args: []string{"2"}, timeout: 1, expectedError: "Error running a command"},
+		{name: "this should be ok on bash too", cmd: "ls", args: []string{"-lh"}, Options: Options{UseShell: true}},
+		{name: "this should abort by timemout", cmd: "sleep", args: []string{"2"}, expectedError: "Error running a command", Options: Options{Timeout: 1}},
 	}
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
 
 			cmd := Command{Command: test.cmd, Args: test.args}
 
-			if test.timeout > 0 {
-				cmd.Options.Timeout = test.timeout
-			}
-
-			if test.useShell {
-				cmd.Options.UseShell = test.useShell
-			}
+			cmd.Options = test.Options
 
 			_, err := RunCmd(cmd)
 
@@ -46,7 +39,7 @@ func TestRunCmd(t *testing.T) {
 
 				if err != nil {
 					// first test if not starts with the message
-					if !strings.HasPrefix(err.Error(), test.expectedError) {
+					if !strings.Contains(err.Error(), test.expectedError) {
 						// if not, then check if its different
 						if err.Error() != test.expectedError {
 							t.Errorf("Expected '%v' but returned '%v", test.expectedError, err)
@@ -118,6 +111,32 @@ func Test_findInPath(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if gotFound := findInPath(tt.args); gotFound != tt.wantFound {
 				t.Errorf("findInPath() = %v, want %v", gotFound, tt.wantFound)
+			}
+		})
+	}
+}
+
+func Test_runCmd(t *testing.T) {
+	type args struct {
+		c Command
+	}
+	tests := []struct {
+		name       string
+		args       args
+		wantOutput []byte
+		wantErr    bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotOutput, err := runCmd(tt.args.c)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("runCmd() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(gotOutput, tt.wantOutput) {
+				t.Errorf("runCmd() = %v, want %v", gotOutput, tt.wantOutput)
 			}
 		})
 	}
